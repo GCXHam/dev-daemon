e810171583c4724e2c552b6b91ebeedc421278ef
ＴＲ <public@technotter.com>
2021-10-24 09:46:18 +0900

    test(*): React系テストとライブラリ系テストで分離したため, それにあわせて設定ファイルを更新

--- jest.config.ts
+++ jest.config.ts
@@ -3,4 +3,6 @@ export default {
   collectCoverage: true,
   coverageDirectory: "coverage",
   coverageProvider: "v8",
+  testEnvironment: "node",
+  testPathIgnorePatterns: ["/node_modules/", "/src/", "/public/", "/build/"],
 };
