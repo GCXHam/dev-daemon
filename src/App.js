169514e10b4fe2ef83e94066e06d49773b17dd16
Yuki992411 <k.yuki.9924.11@gmail.com>
2021-10-24 11:21:11 +0900

    feat(*): サインアップ画面の追加

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import Login from "./Login";
+import SignUp from "./SignUp";
 
 function App() {
   return (
     <Router>
       <Switch>
         <Route path="/" exact component={Login} />
+        <Route path="/signup" exact component={SignUp} />
       </Switch>
     </Router>
   );
