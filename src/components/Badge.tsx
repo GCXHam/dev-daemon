4f809c0f00d6dd3c2ba949e46bb08ea3dd927d4c
ＴＲ <31824852+TetsuOtter@users.noreply.github.com>
2021-12-05 22:34:39 +0900

    style(*): NULL合体演算子を導入

--- src/components/Badge.tsx
+++ src/components/Badge.tsx
@@ -2,8 +2,8 @@ import React from "react";
 import "./Badge.css";
 
 function Badge(props: { title: string, text_color: string, bg_color: string }): JSX.Element {
-  const checkTextColor = props.text_color ? props.text_color : "text-dark";
-  const checkBgColor = props.bg_color ? props.bg_color : "text-white";
+  const checkTextColor = props.text_color ?? "text-dark";
+  const checkBgColor = props.bg_color ?? "text-white";
   return (
     <div className={`badge-style ${checkTextColor} ${checkBgColor}`}>
       {props.title}
--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -2,9 +2,9 @@ import React from "react";
 import "./Button.css";
 
 function Button(props: { title: string, button_size: string, text_color: string, bg_color: string }): JSX.Element {
-  const checkButtonSize = props.button_size ? props.button_size : "";
-  const checkTextColor = props.text_color ? props.text_color : "text-dark";
-  const checkBgColor = props.bg_color ? props.bg_color : "text-white";
+  const checkButtonSize = props.button_size ?? "";
+  const checkTextColor = props.text_color ?? "text-dark";
+  const checkBgColor = props.bg_color ?? "text-white";
 
   return (
     // TODO: if `react-router-dom` is installed, insert `Link` tag.
