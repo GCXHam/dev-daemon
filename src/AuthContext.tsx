e81b58df7f0b5d1bd044fe4a600d46b52a254855
ＴＲ <31824852+TetsuOtter@users.noreply.github.com>
2021-12-05 22:33:31 +0900

    style(*): 不要なimportを削除

--- src/AuthContext.tsx
+++ src/AuthContext.tsx
@@ -1,4 +1,4 @@
-import { createContext, useState, useContext, useEffect, Provider } from "react";
+import { createContext, useState, useContext, useEffect } from "react";
 import { initializeApp } from "firebase/app";
 import { getAuth, onAuthStateChanged, User } from "firebase/auth";
 import { firebaseConfig } from "./FirebaseConfig";
