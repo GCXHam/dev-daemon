e6952ecef739e005ca4b1d2aff74a1d9e5872591
ＴＲ <31824852+TetsuOtter@users.noreply.github.com>
2021-12-05 22:33:02 +0900

    feat(*): 型制約を追加

--- src/AuthContext.tsx
+++ src/AuthContext.tsx
@@ -4,15 +4,25 @@ import { getAuth, onAuthStateChanged, User } from "firebase/auth";
 import { firebaseConfig } from "./FirebaseConfig";
 import { DevDaemonDBController } from "./DevDaemonDBController";
 
-const AuthContext = createContext({});
+interface AuthContextValue {
+  user: User | undefined,
+  db_ctrler: DevDaemonDBController | undefined,
+}
 
-export function useAuthContext(): any {
+const AUTH_CONTEXT_DEFAULT_VALUE: AuthContextValue = {
+  user: undefined,
+  db_ctrler: undefined
+};
+
+const AuthContext = createContext(AUTH_CONTEXT_DEFAULT_VALUE);
+
+export function useAuthContext(): AuthContextValue {
   return useContext(AuthContext);
 }
 
 export function AuthProvider({ children }: { children: JSX.Element }): JSX.Element {
   const [user, setUser] = useState<User>();
-  let db_ctrler = "";
+  const [db_ctrler, setDBCtrler] = useState<DevDaemonDBController>();
 
   const value = {
     user,
